081503a7a3203b28a40f3dbf40c399d8fb422c57
Rupasgolla <85982917+Rupasgolla@users.noreply.github.com>
2024-05-02 19:10:04 +0530

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,7 @@
+# Docker-Installation
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker 
+sudo systemctl enable docker
+sudo systemctl status docker
